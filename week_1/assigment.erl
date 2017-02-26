a53d468e46349fbd94b4b2db9a0e6feb16893661
Bernard Pietraga <bernard.pietraga@gmail.com>
2017-02-26 14:49:42 +0100

    Add comments

--- week_1/assigment.erl
+++ week_1/assigment.erl
@@ -19,11 +19,23 @@ perimeter({polygon, ListOf}) ->
     ListOf.
 
 bits(N) when N >= 0 ->
-    [String]= io_lib:format("~.2B", [N]),
-    count_bits(String).
+    % Format integer to binary number in string format.
+    % Result is in list with string inside,
+    % so I decided to use patter matching to get string out of list.
+    [BinaryString] = io_lib:format("~.2B",[N]),
+    % runs function which returns number of bits
+    % (exactly character 1) in string.
+    count_bits(BinaryString).
 
 count_bits(String) ->
-    F = fun(X, N) when X =:= $1 -> N + 1;
-           (_, N)               -> N
+    % Define anonymous function which adds to accumulator if
+    % character is bit.
+    % If there is nothing more it returns accumulator value.
+    P = fun(Chr,ACC) when Chr=:=$1 -> ACC+1;
+           (_,ACC)                 -> ACC
         end,
-    lists:foldl(F, 0, String).
+    % Now foldl iterates on string and adds to accumulator each bit ocurrance.
+    % foldl is tail recursive.
+    % If I've wanted to make it direct recursive I would have used foldr
+    % which is direct recursive.
+    lists:foldl(P, 0, String).
